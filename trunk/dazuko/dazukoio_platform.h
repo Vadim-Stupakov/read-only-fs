f6d1d241b76656a62b8c18b027bf54c190679a21
Vadim.Stupakov@gmail.com <Vadim.Stupakov@gmail.com@f72b8629-a33e-5070-959e-0171f4cf1310>
2015-02-09 07:28:37 +0000

    added init and deinit functions

--- trunk/dazuko/dazukoio_platform.h
+++
@@ -1,5 +0,0 @@
-#if defined(DUMMYOS)
-	#include "dazukoio_dummyos.h"
-#else
-	#include "dazukoio_unix.h"
-#endif
---
+++ trunk/redirfs/.redirfs.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/vadimstupakov/SVN/read-only-fs/svn/trunk/redirfs/redirfs.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/vadimstupakov/SVN/read-only-fs/svn/trunk/redirfs/redirfs.ko /home/vadimstupakov/SVN/read-only-fs/svn/trunk/redirfs/redirfs.o /home/vadimstupakov/SVN/read-only-fs/svn/trunk/redirfs/redirfs.mod.o
