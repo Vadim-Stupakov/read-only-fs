f08134fc37dec72135cc128ef2b5f33153b6c85f
Vadim-Stupakov <vadim.stupakov@gmail.com>
2015-04-13 21:06:13 +0300

    Removed *cmd files

--- redirfs/.redirfs.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/vadimstupakov/SVN/read-only-fs/svn/trunk/redirfs/redirfs.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/vadimstupakov/SVN/read-only-fs/svn/trunk/redirfs/redirfs.ko /home/vadimstupakov/SVN/read-only-fs/svn/trunk/redirfs/redirfs.o /home/vadimstupakov/SVN/read-only-fs/svn/trunk/redirfs/redirfs.mod.o
