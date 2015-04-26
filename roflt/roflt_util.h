18d360592dceb7367c9b4e21ff719eeffc0175a8
Vadim-Stupakov <vadim.stupakov@gmail.com>
2015-04-26 18:27:44 +0300

    Added the macro #define GET_FILE_LINE_FUNC_ARG __FILE__, __LINE__, __FUNCTION__ and changed the FILTER_LOG_INFO macros

--- roflt/roflt_util.h
+++ roflt/roflt_util.h
@@ -3,20 +3,25 @@
 
 #include <linux/kernel.h>
 
+#define GET_FILE_LINE_FUNC_ARG __FILE__, __LINE__, __FUNCTION__
+
 #define DEBUG 3
 
 #ifdef DEBUG
 
 #if DEBUG > 0
-#define FILTER_LOG_INFO(format, args...) printk(KERN_INFO "%s(%u): "format"\n", (__FUNCTION__), (__LINE__), args )
+#define FILTER_LOG_INFO(format, args...)\
+    printk(KERN_INFO "File:(%s), Line:(%u), Function:(%s)\n\tMessage: "format"\n", GET_FILE_LINE_FUNC_ARG, args)
 #endif
 
 #if DEBUG > 1
-#define FILTER_LOG_DEBUG(format, args...) printk(KERN_DEBUG "%s(%u): "format"\n", (__FUNCTION__), (__LINE__), args )
+#define FILTER_LOG_DEBUG(format, args...)\
+    printk(KERN_DEBUG "File:(%s), Line:(%u), Function:(%s)\n\tMessage: "format"\n", GET_FILE_LINE_FUNC_ARG, args)
 #endif
 
 #if DEBUG > 2
-#define FILTER_LOG_CRIT(format, args...) printk(KERN_CRIT "%s(%u): "format"\n", (__FUNCTION__), (__LINE__), args )
+#define FILTER_LOG_CRIT(format, args...)\
+    printk(KERN_CRIT "File:(%s), Line:(%u), Function:(%s)\n\tMessage: "format"\n", GET_FILE_LINE_FUNC_ARG, args)
 #endif
 
 #endif //#ifdef DEBUG
